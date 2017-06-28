f59d8b30547bb6ef53018165532c9556b36db7b7
Augusto <augusto.jm.amaral1@gmail.com>
2017-06-28 18:08:32 -0300

    Add middlewares

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const app  = require('./src/app'),
-      port = app.get('PORT')
+      port = app.get('port')
 
 module.exports = app.listen(port, () => {
   console.log(`Listening on port ${port}`)
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,18 @@
 'use strict'
 
-const config = require('config')
+const config      = require('config'),
+      bodyParser  = require('body-parser'),
+      cors        = require('cors'),
+      helmet      = require('helmet'),
+      compression = require('compression')
 
 module.exports = (app) => {
-  app.set('PORT', config.PORT)
+
+  app.set('port', config.port)
+
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(helmet())
+  app.use(compression())
 }
